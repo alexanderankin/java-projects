d7ffd8bf4759f96efdf00bfb3a7adf60e006f2a8
David Ankin <daveankin@gmail.com>
2022-05-10 15:23:57 -0400

    add supports-hyperlinks

---
+++ terminal/supports-hyperlinks/src/main/java/info/ankin/projects/supports_hyperlinks/SupportsHyperlinks.java
@@ -0,0 +1,7 @@
+package info.ankin.projects.supports_hyperlinks;
+
+/**
+ * Main entrypoint to this artifact
+ */
+public class SupportsHyperlinks {
+}
---
+++ terminal/supports-hyperlinks/src/main/java/info/ankin/projects/supports_hyperlinks/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * package for dealing with terminal hyperlink capability,
+ * inspired by <a href="https://github.com/zkat/supports-hyperlinks">zkat/supports-hyperlinks</a>.
+ */
+package info.ankin.projects.supports_hyperlinks;
