5ae8a5b7972100970e2dde74153fa7ba3395bbee
David Ankin <daveankin@gmail.com>
2022-07-21 07:16:29 -0400

    write some code in the app project, todo: openssl, move into library

--- lib/spring/app/src/main/java/info/ankin/projects/spring/app/Application.java
+++ lib/spring/app/src/main/java/info/ankin/projects/spring/app/Application.java
@@ -2,10 +2,52 @@ package info.ankin.projects.spring.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.server.Ssl;
+import org.springframework.boot.web.server.SslStoreProvider;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+import java.security.KeyStore;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Configuration
+    public static class Config {
+        public WebServerFactoryCustomizer<NettyReactiveWebServerFactory> customizer() {
+            return new WebServerFactoryCustomizer<>() {
+                @Override
+                public void customize(NettyReactiveWebServerFactory factory) {
+                    factory.setSsl(new Ssl());
+                    factory.setSslStoreProvider(new SingleCertSslStoreProvider(getKeyStore()));
+                }
+
+                private KeyStore getKeyStore() {
+                    return new OpenSslRunner().runOpenSsl();
+                }
+            };
+        }
+    }
+
+    public static class SingleCertSslStoreProvider implements SslStoreProvider {
+        private final KeyStore keyStore;
+
+        public SingleCertSslStoreProvider(KeyStore keyStore) {
+            this.keyStore = keyStore;
+        }
+
+        @Override
+        public KeyStore getKeyStore() {
+            return keyStore;
+        }
+
+        @Override
+        public KeyStore getTrustStore() {
+            return null;
+        }
     }
 }
---
+++ lib/spring/app/src/main/java/info/ankin/projects/spring/app/OpenSslRunner.java
@@ -0,0 +1,9 @@
+package info.ankin.projects.spring.app;
+
+import java.security.KeyStore;
+
+public class OpenSslRunner {
+    public KeyStore runOpenSsl() {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+}
