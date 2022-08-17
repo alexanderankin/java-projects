c7d32debd08f2784f32302c4e707076b0d69373a
David Ankin <daveankin@gmail.com>
2022-08-17 17:21:10 -0400

    start htpasswd cli

---
+++ cli/htpasswd-cli/src/main/java/info/ankin/projects/cli/htpasswd/exception/MultipleModesException.java
@@ -0,0 +1,7 @@
+package info.ankin.projects.cli.htpasswd.exception;
+
+public class MultipleModesException extends HtpasswdCliException {
+    public MultipleModesException(String message) {
+        super(message);
+    }
+}
