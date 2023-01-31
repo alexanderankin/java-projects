f2391a4ed7c93f3cbccc885c9573302d5484d63d
David Ankin <daveankin@gmail.com>
2023-01-30 21:59:49 -0500

    cli daemon example

---
+++ cli/daemon-example/src/main/java/info/ankin/projects/cli/daemonexample/DaemonExample.java
@@ -0,0 +1,51 @@
+package info.ankin.projects.cli.daemonexample;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.daemon.Daemon;
+import org.apache.commons.daemon.DaemonContext;
+import org.apache.commons.daemon.DaemonInitException;
+
+@Slf4j
+public class DaemonExample implements Daemon {
+    Thread backgroundThread;
+
+    public static void main(String[] args) throws Exception {
+        new DaemonExample().start();
+    }
+
+    @Override
+    public void init(DaemonContext context) throws DaemonInitException, Exception {
+        log.info("init");
+    }
+
+    @Override
+    public void start() throws Exception {
+        log.info("start");
+
+        backgroundThread = new Thread(() -> {
+            int i = 0;
+            while (true) {
+                i++;
+                log.info("we are now on: {}", i);
+                try {
+                    //noinspection BusyWait
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+                    break;
+                }
+            }
+        });
+        backgroundThread.start();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        log.info("stop");
+        if (backgroundThread != null) backgroundThread.interrupt();
+    }
+
+    @Override
+    public void destroy() {
+        log.info("destroy");
+    }
+}
