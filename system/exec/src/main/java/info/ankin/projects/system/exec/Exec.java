a5bf50b103d83371a9b4bcdcd167ded776b97b0b
David Ankin <daveankin@gmail.com>
2026-07-07 09:16:13 -0400

    more cleanup+polish on exec

--- system/exec/src/main/java/info/ankin/projects/system/exec/Exec.java
+++ system/exec/src/main/java/info/ankin/projects/system/exec/Exec.java
@@ -3,15 +3,17 @@ package info.ankin.projects.system.exec;
 import lombok.*;
 import lombok.experimental.Accessors;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.output.NullOutputStream;
+import org.apache.commons.io.output.TeeOutputStream;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -61,22 +63,62 @@ public class Exec {
 
         var process = pb.start();
 
-        if (captureOutput) {
-        }
+        var out = pb.redirectOutput() == ProcessBuilder.Redirect.PIPE
+                ? handleStream(captureOutput, processConfig.getStandardOutput(), process.getInputStream(), System.out)
+                : RunningProcess.CaptureStreamHandle.NULL;
+        var err = pb.redirectError() == ProcessBuilder.Redirect.PIPE
+                ? handleStream(captureOutput, processConfig.getStandardError(), process.getErrorStream(), System.err)
+                : RunningProcess.CaptureStreamHandle.NULL;
 
         return new RunningProcess()
                 .setConfig(processConfig)
-                .setProcess(process);
+                .setProcess(process)
+                .setStandardOutputStreamHandle(out)
+                .setStandardErrorStreamHandle(err);
     }
 
-    @ToString(callSuper = true)
-    @EqualsAndHashCode(callSuper = true)
+    @SneakyThrows
+    private RunningProcess.CaptureStreamHandle handleStream(boolean captureOutput,
+                                                            ProcessConfig.ProcessStream.ToStream toStream,
+                                                            InputStream original,
+                                                            OutputStream inheritTarget) {
+        var target = switch (toStream) {
+            case ProcessConfig.ProcessStream.DiscardProcessStream ignored -> NullOutputStream.INSTANCE;
+            case ProcessConfig.ProcessStream.InheritedProcessStream ignored -> inheritTarget;
+            case ProcessConfig.ProcessStream.PipeProcessStream.FilePipeProcessStream filePipeProcessStream ->
+                    Files.newOutputStream(filePipeProcessStream.getFile());
+            case ProcessConfig.ProcessStream.PipeProcessStream.ProcessOutputToStream processOutputToStream ->
+                    processOutputToStream.getOutputStream();
+        };
+
+        ByteArrayOutputStream capture;
+        OutputStream stream;
+        if (captureOutput) {
+            capture = new ByteArrayOutputStream();
+            stream = new TeeOutputStream(target, capture);
+        } else {
+            capture = null;
+            stream = target;
+        }
+        var thread = Thread.ofVirtual().name(getClass().getSimpleName() + "-transfer-stream").start(() -> transfer(original, stream));
+        return new RunningProcess.CaptureStreamHandle(capture, thread);
+    }
+
+    @SneakyThrows
+    private void transfer(InputStream inputStream, OutputStream finalOutTarget) {
+        inputStream.transferTo(finalOutTarget);
+    }
+
     @Data
     @Accessors(chain = true)
-    public static class CompletedProcess extends RunningProcess {
+    public static class CompletedProcess {
         int exitCode;
+        ProcessConfig config;
+        Process process;
         byte[] standardOutput;
         byte[] standardError;
+        String standardOutputString;
+        String standardErrorString;
     }
 
     @Data
@@ -84,9 +126,42 @@ public class Exec {
     public static class RunningProcess {
         ProcessConfig config;
         Process process;
+        CaptureStreamHandle standardOutputStreamHandle;
+        CaptureStreamHandle standardErrorStreamHandle;
 
         @SneakyThrows
         public CompletedProcess waitUntilCompletion() {
+            waitForProcess(process);
+            waitForThread(standardOutputStreamHandle.thread);
+            waitForThread(standardErrorStreamHandle.thread);
+
+            var standardOutputStream = standardOutputStreamHandle.stream;
+            var standardErrorStream = standardErrorStreamHandle.stream;
+
+            var completedProcess = new CompletedProcess()
+                    .setExitCode(process.exitValue())
+                    .setStandardOutput(standardOutputStream == null ? null : standardOutputStream.toByteArray())
+                    .setStandardError(standardErrorStream == null ? null : standardErrorStream.toByteArray());
+
+            if (completedProcess.getStandardOutput() != null && config.getStandardOutputCharset() != null)
+                completedProcess.setStandardOutputString(new String(completedProcess.getStandardOutput(), config.getStandardOutputCharset()));
+
+            if (completedProcess.getStandardError() != null && config.getStandardErrorCharset() != null)
+                completedProcess.setStandardErrorString(new String(completedProcess.getStandardError(), config.getStandardErrorCharset()));
+
+            completedProcess
+                    .setConfig(config)
+                    .setProcess(process);
+
+            if (!config.allowedExitCodes.contains(completedProcess.getExitCode())) {
+                throw new ExecExitCodeException().setCompletedProcess(completedProcess);
+            }
+
+            return completedProcess;
+        }
+
+        @SneakyThrows
+        private void waitForProcess(Process process) {
             if (config.timeout == null) {
                 process.waitFor();
             } else {
@@ -95,36 +170,71 @@ public class Exec {
                         case ProcessConfig.TimeoutAction.No ignored -> {
                         }
                         case ProcessConfig.TimeoutAction.Throw ignored ->
-                                throw new ExecTimeoutException().setProcessConfig(config);
-                        case ProcessConfig.TimeoutAction.SigTermThenSigKillThenThrow termKillThrow -> {
-                            process.destroy();
+                                throw new ExecTimeoutException().setRunningProcess(this);
+                        case ProcessConfig.TimeoutAction.SigTermThenKillThenThrow termKillThrow -> {
                             if (!process.waitFor(TimeUnit.NANOSECONDS.convert(termKillThrow.timeoutAfterSigTerm), TimeUnit.NANOSECONDS)) {
                                 process.destroyForcibly();
                                 if (!process.waitFor(TimeUnit.NANOSECONDS.convert(termKillThrow.timeoutAfterSigKill), TimeUnit.NANOSECONDS)) {
-                                    throw new ExecTimeoutException().setProcessConfig(config);
+                                    throw new ExecTimeoutException().setRunningProcess(this);
                                 }
                             }
                         }
                         case ProcessConfig.TimeoutAction.SigTermThenThrow termThenThrow -> {
+                            process.destroy();
                             if (!process.waitFor(TimeUnit.NANOSECONDS.convert(termThenThrow.timeoutAfterSigTerm), TimeUnit.NANOSECONDS)) {
-                                throw new ExecTimeoutException().setProcessConfig(config);
+                                throw new ExecTimeoutException().setRunningProcess(this);
                             }
                         }
                     }
                 }
             }
+        }
 
-            var completedProcess = new CompletedProcess();
-            completedProcess
-                    .setConfig(config)
-                    .setProcess(process);
-            return completedProcess.setExitCode(process.exitValue());
+        @SneakyThrows
+        private void waitForThread(Thread thread) {
+            if (thread == null)
+                return;
+            if (config.timeout == null) {
+                thread.join();
+            } else {
+                thread.join(config.timeout.toMillis());
+                if (thread.isAlive() && thread.getState() != Thread.State.TERMINATED) {
+                    switch (config.timeoutAction) {
+                        case ProcessConfig.TimeoutAction.No ignored -> {
+                        }
+                        case ProcessConfig.TimeoutAction.Throw ignored ->
+                                throw new ExecTimeoutException().setRunningProcess(this);
+                        case ProcessConfig.TimeoutAction.SigTermThenKillThenThrow termKillThrow -> {
+                            thread.interrupt();
+                            thread.join(termKillThrow.timeoutAfterSigTerm.toMillis());
+                            if (thread.isAlive() && thread.getState() != Thread.State.TERMINATED) {
+                                thread.interrupt();
+                                thread.join(termKillThrow.timeoutAfterSigKill.toMillis());
+                                if (thread.isAlive() && thread.getState() != Thread.State.TERMINATED) {
+                                    throw new ExecTimeoutException().setRunningProcess(this);
+                                }
+                            }
+                        }
+                        case ProcessConfig.TimeoutAction.SigTermThenThrow termThenThrow -> {
+                            thread.interrupt();
+                            if (!thread.join(termThenThrow.timeoutAfterSigTerm)) {
+                                throw new ExecTimeoutException().setRunningProcess(this);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        record CaptureStreamHandle(ByteArrayOutputStream stream, Thread thread) {
+            static final CaptureStreamHandle NULL = new CaptureStreamHandle(null, null);
         }
     }
 
     @Data
     @Accessors(chain = true)
     public static class ProcessConfig {
+        public static final Set<Integer> DEFAULT_ALLOWED_EXIT_CODES = Set.of(0);
         Path workingDirectory;
         String command;
         List<String> arguments;
@@ -134,7 +244,10 @@ public class Exec {
         ProcessStream.ToStream standardOutput = ProcessStream.InheritedProcessStream.INSTANCE;
         ProcessStream.ToStream standardError = ProcessStream.InheritedProcessStream.INSTANCE;
         Duration timeout;
-        TimeoutAction timeoutAction = new TimeoutAction.SigTermThenSigKillThenThrow();
+        TimeoutAction timeoutAction = new TimeoutAction.SigTermThenKillThenThrow();
+        Charset standardOutputCharset;
+        Charset standardErrorCharset;
+        Set<Integer> allowedExitCodes = DEFAULT_ALLOWED_EXIT_CODES;
 
         //<editor-fold desc="stream helper setters">
         public ProcessConfig inheritStandardInput() {
@@ -152,18 +265,18 @@ public class Exec {
             return this;
         }
 
-        public ProcessConfig streamStandardInput(OutputStream stream) {
-            standardInput = new ProcessStream.PipeProcessStream.PipeProcessStream.ProcessInputFromStream().setOutputStream(stream);
+        public ProcessConfig streamStandardInput(InputStream stream) {
+            standardInput = new ProcessStream.PipeProcessStream.PipeProcessStream.ProcessInputFromStream().setInputStream(stream);
             return this;
         }
 
-        public ProcessConfig streamStandardOutput(InputStream stream) {
-            standardOutput = new ProcessStream.PipeProcessStream.PipeProcessStream.ProcessOutputToStream().setInputStream(stream);
+        public ProcessConfig streamStandardOutput(OutputStream stream) {
+            standardOutput = new ProcessStream.PipeProcessStream.PipeProcessStream.ProcessOutputToStream().setOutputStream(stream);
             return this;
         }
 
-        public ProcessConfig streamStandardError(InputStream stream) {
-            standardError = new ProcessStream.PipeProcessStream.PipeProcessStream.ProcessOutputToStream().setInputStream(stream);
+        public ProcessConfig streamStandardError(OutputStream stream) {
+            standardError = new ProcessStream.PipeProcessStream.PipeProcessStream.ProcessOutputToStream().setOutputStream(stream);
             return this;
         }
         //</editor-fold>
@@ -219,7 +332,7 @@ public class Exec {
                 @Data
                 @Accessors(chain = true)
                 public static final class ProcessInputFromStream extends PipeProcessStream implements FromStream {
-                    OutputStream outputStream;
+                    InputStream inputStream;
                 }
 
                 @ToString(callSuper = true)
@@ -227,7 +340,7 @@ public class Exec {
                 @Data
                 @Accessors(chain = true)
                 public static final class ProcessOutputToStream extends PipeProcessStream implements ToStream {
-                    InputStream inputStream;
+                    OutputStream outputStream;
                 }
             }
 
@@ -241,6 +354,7 @@ public class Exec {
             @Data
             @Accessors(chain = true)
             public static final class No extends TimeoutAction {
+                public static final No INSTANCE = new No();
             }
 
             @ToString(callSuper = true)
@@ -248,13 +362,15 @@ public class Exec {
             @Data
             @Accessors(chain = true)
             public static final class Throw extends TimeoutAction {
+                public static final Throw INSTANCE = new Throw();
             }
 
             @ToString(callSuper = true)
             @EqualsAndHashCode(callSuper = true)
             @Data
             @Accessors(chain = true)
-            public static sealed class SigTermThenThrow extends TimeoutAction permits SigTermThenSigKillThenThrow {
+            public static sealed class SigTermThenThrow extends TimeoutAction permits SigTermThenKillThenThrow {
+                public static final SigTermThenThrow DEFAULT = new SigTermThenThrow();
                 Duration timeoutAfterSigTerm = Duration.ofSeconds(5);
             }
 
@@ -262,20 +378,33 @@ public class Exec {
             @EqualsAndHashCode(callSuper = true)
             @Data
             @Accessors(chain = true)
-            public static final class SigTermThenSigKillThenThrow extends SigTermThenThrow {
+            public static final class SigTermThenKillThenThrow extends SigTermThenThrow {
+                public static final SigTermThenKillThenThrow DEFAULT = new SigTermThenKillThenThrow();
                 Duration timeoutAfterSigKill = Duration.ofSeconds(5);
             }
         }
     }
 
-    @Getter
-    @Setter
-    @ToString
-    @Accessors(chain = true)
     public static abstract class ExecException extends RuntimeException {
-        ProcessConfig processConfig;
     }
 
+    @Getter
+    @Setter
+    @ToString(callSuper = true)
+    @Accessors(chain = true)
     public static class ExecTimeoutException extends ExecException {
+        RunningProcess runningProcess;
+    }
+
+    @Getter
+    @Setter
+    @ToString(callSuper = true)
+    @Accessors(chain = true)
+    public static class ExecExitCodeException extends ExecException {
+        CompletedProcess completedProcess;
+
+        public int getExitCode() {
+            return completedProcess.getExitCode();
+        }
     }
 }
--- system/exec/src/test/java/info/ankin/projects/system/exec/ExecTest.java
+++ system/exec/src/test/java/info/ankin/projects/system/exec/ExecTest.java
@@ -1,10 +1,27 @@
 package info.ankin.projects.system.exec;
 
+import info.ankin.projects.system.exec.Exec.ProcessConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class ExecTest {
+    Exec exec;
+
+    @BeforeEach
+    void setUp() {
+        exec = new Exec();
+    }
+
     @ParameterizedTest
     @CsvSource({
             "'cat README.md'",
@@ -12,5 +29,32 @@ class ExecTest {
     })
     void test() {
 
+    }
+
+    @Test
+    void test_basic() {
+        var completedProcess = exec.run(new ProcessConfig()
+                .setCaptureOutput(true)
+                .setStandardOutputCharset(StandardCharsets.UTF_8)
+                .setCommand("whoami"));
+        // System.out.println(completedProcess);
+        assertTrue(completedProcess.getStandardOutputString().contains(System.getProperty("user.name")));
+
+
+        exec.run(new ProcessConfig().setCommand("whoami").setStandardOutput(ProcessConfig.ProcessStream.DiscardProcessStream.INSTANCE));
+        System.out.println(Arrays.toString(exec.run(new ProcessConfig().setCommand("whoami").setStandardOutput(ProcessConfig.ProcessStream.DiscardProcessStream.INSTANCE).setCaptureOutput(true)).getStandardOutput()));
+    }
+
+    @Test
+    void longRunningTest() {
+        var outputStream = new ByteArrayOutputStream();
+        var bash = exec.run(new ProcessConfig()
+                .setCommand("bash")
+                .setArguments(List.of("-c", "for i in {1..10000}; do printf $i; sleep 1; done"))
+                .setStandardOutput(new ProcessConfig.ProcessStream.PipeProcessStream.ProcessOutputToStream().setOutputStream(outputStream))
+                .setTimeout(Duration.ofSeconds(5))
+                .setTimeoutAction(ProcessConfig.TimeoutAction.SigTermThenThrow.DEFAULT));
+        System.out.println(outputStream.size());
+        System.out.println(bash);
     }
 }
